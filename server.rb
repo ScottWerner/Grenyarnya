1626788dbcf7f0dbf0a74958f08a4a5eb1b64501
ScottWerner <scott@gratifyingoverhaul.com>
2009-08-28 15:28:46 -0400

    changes to server file

--- server.rb
+++ server.rb
@@ -6,6 +6,9 @@ require 'rubygems'
 
 require 'lib/client'
 require 'lib/chat'
+require 'lib/world'
+require 'lib/area'
+require 'lib/room'
 
 
 
@@ -15,13 +18,14 @@ Thread.abort_on_exception = config['debug']
 puts "Starting the server..."
 server = TCPServer.new(config['port'])
 chat = Chat.new()
+world = World.new
 threads = []
 
 while (session = server.accept)
 	#log connection and address
 	threads << Thread.new(session) { |s|
 			puts "Accepting connection from #{s.addr[2]}."
-			c = Client.new(s,chat)
+			c = Client.new(s,chat, world)
 			Thread.current['client'] = c
 			c.start
 			s.close
